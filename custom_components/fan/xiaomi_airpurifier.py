98090451862673e4c48c6617675f88543e7ffaa1
Sebastian Muszynski <s.muszynski@constructiva.de>
2017-09-05 07:52:37 +0200

    Set favorite level fixed at python-mirobo (e6c49f978517dc27c9a7fe9702fa4c5fafe93d2d).

--- custom_components/fan/xiaomi_airpurifier.py
+++ custom_components/fan/xiaomi_airpurifier.py
@@ -34,7 +34,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 # REQUIREMENTS = ['python-mirobo']
 REQUIREMENTS = ['https://github.com/rytilahti/python-mirobo/archive/'
-                '168f5c0ff381b3b02cedd0917597195b3c521a20.zip#'
+                'e6c49f978517dc27c9a7fe9702fa4c5fafe93d2d.zip#'
                 'python-mirobo']
 
 ATTR_TEMPERATURE = 'temperature'
