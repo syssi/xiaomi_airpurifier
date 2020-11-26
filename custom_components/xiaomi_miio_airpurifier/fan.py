7011a3a2edd84b7615d947331fbe825902183fec
Sebastian Muszynski <basti@linkt.de>
2020-11-26 08:34:11 +0100

    Wire the async_set_wet_protection_{on,off} methods properly to the service (Closes: #106)

--- custom_components/xiaomi_miio_airpurifier/fan.py
+++ custom_components/xiaomi_miio_airpurifier/fan.py
@@ -932,8 +932,8 @@ SERVICE_TO_METHOD = {
     SERVICE_SET_PTC_OFF: {"method": "async_set_ptc_off"},
     SERVICE_SET_DISPLAY_ON: {"method": "async_set_display_on"},
     SERVICE_SET_DISPLAY_OFF: {"method": "async_set_display_off"},
-    SERVICE_SET_WET_PROTECTION_ON: {"method": "async_set_wet_protection"},
-    SERVICE_SET_WET_PROTECTION_OFF: {"method": "async_set_wet_protection"},
+    SERVICE_SET_WET_PROTECTION_ON: {"method": "async_set_wet_protection_on"},
+    SERVICE_SET_WET_PROTECTION_OFF: {"method": "async_set_wet_protection_off"},
 }
 
 
