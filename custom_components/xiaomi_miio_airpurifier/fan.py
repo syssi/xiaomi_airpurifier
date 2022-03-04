41c00a9398c3483df1c83fecc042c8327e8af8b9
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2022-03-04 16:32:29 +0300

    Add deerma.humidifier.jsqs support (#259)

--- custom_components/xiaomi_miio_airpurifier/fan.py
+++ custom_components/xiaomi_miio_airpurifier/fan.py
@@ -142,6 +142,7 @@ MODEL_AIRHUMIDIFIER_MJJSQ = "deerma.humidifier.mjjsq"
 MODEL_AIRHUMIDIFIER_JSQ = "deerma.humidifier.jsq"
 MODEL_AIRHUMIDIFIER_JSQ1 = "deerma.humidifier.jsq1"
 MODEL_AIRHUMIDIFIER_JSQ5 = "deerma.humidifier.jsq5"
+MODEL_AIRHUMIDIFIER_JSQS = "deerma.humidifier.jsqs"
 MODEL_AIRHUMIDIFIER_JSQ001 = "shuii.humidifier.jsq001"
 
 MODEL_AIRFRESH_A1 = "dmaker.airfresh.a1"
@@ -199,6 +200,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
                 MODEL_AIRHUMIDIFIER_JSQ,
                 MODEL_AIRHUMIDIFIER_JSQ1,
                 MODEL_AIRHUMIDIFIER_JSQ5,
+                MODEL_AIRHUMIDIFIER_JSQS,
                 MODEL_AIRHUMIDIFIER_JSQ001,
                 MODEL_AIRFRESH_A1,
                 MODEL_AIRFRESH_VA2,
@@ -277,7 +279,7 @@ ATTR_FAULT = "fault"
 ATTR_POWER_TIME = "power_time"
 ATTR_CLEAN_MODE = "clean_mode"
 
-# Air Humidifier MJJSQ, JSQ and JSQ1 and JSQ5
+# Air Humidifier MJJSQ, JSQ, JSQ1, JSQ5 ans JSQS
 ATTR_NO_WATER = "no_water"
 ATTR_WATER_TANK_DETACHED = "water_tank_detached"
 ATTR_WET_PROTECTION = "wet_protection"
@@ -532,6 +534,11 @@ AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQ5 = {
     ATTR_LED: "led_light",
     ATTR_NO_WATER: "water_shortage_fault",
     ATTR_WATER_TANK_DETACHED: "tank_filed",
+}
+
+AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQS = {
+    **AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQ5,
+    ATTR_WET_PROTECTION: "overwet_protect",
 }
 
 AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQ = {
@@ -837,6 +844,13 @@ FEATURE_FLAGS_AIRHUMIDIFIER_JSQ5 = (
     FEATURE_SET_BUZZER | FEATURE_SET_LED | FEATURE_SET_TARGET_HUMIDITY
 )
 
+FEATURE_FLAGS_AIRHUMIDIFIER_JSQS = (
+    FEATURE_SET_BUZZER
+    | FEATURE_SET_LED
+    | FEATURE_SET_TARGET_HUMIDITY
+    | FEATURE_SET_WET_PROTECTION
+)
+
 FEATURE_FLAGS_AIRHUMIDIFIER_JSQ = (
     FEATURE_SET_BUZZER
     | FEATURE_SET_LED
@@ -1134,7 +1148,10 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     ]:
         air_humidifier = AirHumidifierMjjsq(host, token, model=model)
         device = XiaomiAirHumidifierMjjsq(name, air_humidifier, model, unique_id)
-    elif model == MODEL_AIRHUMIDIFIER_JSQ5:
+    elif model in [
+        MODEL_AIRHUMIDIFIER_JSQ5,
+        MODEL_AIRHUMIDIFIER_JSQS,
+    ]:
         air_humidifier = AirHumidifierJsqs(host, token, model=model)
         device = XiaomiAirHumidifierJsqs(name, air_humidifier, model, unique_id)
     elif model == MODEL_AIRHUMIDIFIER_JSQ001:
@@ -1942,8 +1959,12 @@ class XiaomiAirHumidifierJsqs(XiaomiAirHumidifier):
         """Initialize the plug switch."""
         super().__init__(name, device, model, unique_id)
 
-        self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER_JSQ5
-        self._available_attributes = AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQ5
+        if self._model == MODEL_AIRHUMIDIFIER_JSQ5:
+            self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER_JSQ5
+            self._available_attributes = AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQ5
+        else:
+            self._device_features = FEATURE_FLAGS_AIRHUMIDIFIER_JSQS
+            self._available_attributes = AVAILABLE_ATTRIBUTES_AIRHUMIDIFIER_JSQS
 
         self._preset_modes = [mode.name for mode in AirhumidifierJsqsOperationMode]
         self._state_attrs = {ATTR_MODEL: self._model}
