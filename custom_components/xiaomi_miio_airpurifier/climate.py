55d330426fa8cf16550a31556d0c8c0ab2ee241a
shbatm <support@shbatm.com>
2025-09-25 06:57:49 -0500

    Update climate.py to use same unique_id format

--- custom_components/xiaomi_miio_airpurifier/climate.py
+++ custom_components/xiaomi_miio_airpurifier/climate.py
@@ -139,7 +139,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     model = config.get(CONF_MODEL)
 
     _LOGGER.info("Initializing with host %s (token %s...)", host, token[:5])
-    unique_id = None
+    unique_id = f"{host}-{token[:5]}"
 
     if model is None:
         miio_device = Device(host, token)
@@ -149,7 +149,6 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
             raise PlatformNotReady
 
         model = device_info.model
-        unique_id = "{}-{}".format(model, device_info.mac_address)
         _LOGGER.info(
             "%s %s %s detected",
             model,
